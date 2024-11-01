92f336dc43851710ddf591ce5e7b150e50bc82a5
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-11-01 14:36:57 +0200

    Ürituse lisamise vaade tehtud!

--- NullamGuestRegistrationSystemSolution/WebApp/ViewModels/LisaMuudaÜritusVM.cs
+++ NullamGuestRegistrationSystemSolution/WebApp/ViewModels/LisaMuudaÜritusVM.cs
@@ -6,14 +6,29 @@ namespace WebApp.ViewModels
     public class LisaMuudaÜritusVM
     {
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(64)]
-        [StringLength(64)]
+        [Display(Name = "Ürituse Nimi")]
+        [Required(ErrorMessage = "Väli Ürituse Nimi on kohustuslik!")]
+        [MaxLength(64, ErrorMessage = "Väljale sisestava teksti maksimaalne pikkus on 64 tähemärki!")]
+        [StringLength(64, ErrorMessage = "Väljale sisestava teksti maksimaalne pikkus peab jääma vahemikku 1 kuni 64 tähemärki!")]
         public string ÜrituseNimi { get; set; } = default!;
+
+        [Required(ErrorMessage = "Väli Toimumisaeg on kohustuslik!")]
         [DataType(DataType.DateTime)]
         [DisplayFormat(DataFormatString = "{0:g}", ApplyFormatInEditMode = false)]
         public string Toimumisaeg { get; set; } = default!;
+
+        [Required(ErrorMessage = "Väli Koht on kohustuslik!")]
+        [MaxLength(64, ErrorMessage = "Väljale sisestava teksti maksimaalne pikkus on 64 tähemärki!")]
+        [StringLength(64, ErrorMessage = "Väljale sisestava teksti pikkus peab jääma vahemikku 1 kuni 64 tähemärki!")]
+        public string Koht { get; set; } = default!;
+
+        [MaxLength(1000, ErrorMessage = "Väljale sisestava teksti maksimaalne pikkus on 1000 tähemärki!")]
+        [DataType(DataType.MultilineText)]
+        public string? Lisainfo { get; set; }
+
+
+
+
 
     }
 }
