eda19cb7537cae49bb1982bc79e5df0072615220
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-11-30 19:52:24 +0200

    Adding error message to requred atripute to eventId in AddAttendeeToAnotherEventVM.

--- NullamGuestRegistrationSystemSolution/WebApp/ViewModels/AddAttendeeToAnotherEventVM.cs
+++ NullamGuestRegistrationSystemSolution/WebApp/ViewModels/AddAttendeeToAnotherEventVM.cs
@@ -3,23 +3,27 @@ using App.Enum;
 using Base.Domain;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.ViewModels
 {
     public class AddAttendeeToAnotherEventVM : DomainEntityId<int>
     {
-
         public int AttendeeId { get; set; }
         
         public AttendeeType? AttendeeType { get; set; }
+
+
         [DisplayName("Osavõtjate arvu muutmine?")]
         public bool IsNumberOfPeopleFromCompanyChanged { get; set; }
+
 
         [DisplayName("Ettevõtest tulevate osavõtjate arv")]
         public int? NumberOfPeopleFromCompany { get; set; }
 
-        [DisplayName("Üritus")]
 
+        [Required(ErrorMessage = "Väli Üritus on kohustuslik!")]
+        [DisplayName("Üritus")]
         public int EventId { get; set; }
 
         public SelectList? Events { get; set; }
